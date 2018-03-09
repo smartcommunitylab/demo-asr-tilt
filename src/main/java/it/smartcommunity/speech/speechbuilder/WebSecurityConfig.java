6a9d4e0ae02d5ec8a252754433204c2e2c4d9f91
kazhamiakin <kazhamiakin@gmail.com>
2018-03-09 17:36:50 +0100

    disable csrf

--- src/main/java/it/smartcommunity/speech/speechbuilder/WebSecurityConfig.java
+++ src/main/java/it/smartcommunity/speech/speechbuilder/WebSecurityConfig.java
@@ -39,6 +39,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+        	.csrf()
+        		.disable()
             .authorizeRequests()
                 .antMatchers("/","/css/**", "/js/**", "/fonts/**", "/img/**", "/api/words").permitAll()
                 .anyRequest().authenticated()
