6135205c6b982b93b01a4b56ae11f79cba06b6c4
kazhamiakin <kazhamiakin@gmail.com>
2018-03-22 10:22:37 +0100

    ui and cloud

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -8,10 +8,10 @@ $(document).ready(function() {
     
     var textChunk = '';
     
-    $('#keywords').jQCloud([], {
-    	  width: 500,
-    	  height: 350
-	});
+//    $('#keywords').jQCloud([], {
+//    	  width: 500,
+//    	  height: 350
+//	});
     
     /**
      * Send extracted text to word cloud builder
@@ -48,11 +48,12 @@ $(document).ready(function() {
     		success: function(data) {
     			viewTimer = null;
     			var words = [];
-    			data.forEach(function(d) {words.push({text: d.name, weight: d.value})});
+//    			data.forEach(function(d) {words.push({text: d.name, weight: d.value})});
 //    			for (var key in data) {
 //    				words.push({text: key, weight: data[key]});
 //    			}
-    			$('#keywords').jQCloud('update', words);
+//    			$('#keywords').jQCloud('update', words);
+    			createCloud(data);
     			console.log('data', data);
         	}
     	});
@@ -245,7 +246,7 @@ $(document).ready(function() {
             contentType: false,
             processData: false,
             success: function() {
-              $('#success').html("Operazione e' andata a buon fine!").show();
+              $('#success').html("Operazione e' andata a buon fine! Il file sara' processato.").show();
               setTimeout(function() {
             	  $('#success').hide();
               }, 3000);
@@ -259,4 +260,46 @@ $(document).ready(function() {
         });
     });
 
+    function createCloud(data) {
+    	var chart = echarts.init(document.getElementById('keywords'));
+    	chart.setOption({
+    	    series: [{
+    	        type: 'wordCloud',
+    	        shape: 'circle',
+    	        left: 'center',
+    	        top: 'center',
+    	        width: '70%',
+    	        height: '100%',
+    	        right: null,
+    	        bottom: null,
+    	        sizeRange: [24, 100],
+    	        rotationRange: [-90, 90],
+    	        rotationStep: 45,
+    	        gridSize: 10,
+    	        drawOutOfBound: false,
+    	        textStyle: {
+    	            normal: {
+    	                fontFamily: 'sans-serif',
+    	                fontWeight: 'bold',
+    	                // Color can be a callback function or a color string
+    	                color: function () {
+    	                    // Random color
+    	                    return 'rgb(' + [
+    	                        Math.round(Math.random() * 160),
+    	                        Math.round(Math.random() * 160),
+    	                        Math.round(Math.random() * 160)
+    	                    ].join(',') + ')';
+    	                }
+    	            },
+    	            emphasis: {
+    	                shadowBlur: 10,
+    	                shadowColor: '#333'
+    	            }
+    	        },
+    	        data: data
+    	    }]
+    	});
+    	
+    }
+    
 });
--- src/main/resources/static/js/view.js
+++ src/main/resources/static/js/view.js
@@ -2,10 +2,10 @@ var lastResult;
 
 $(document).ready(function() {
     
-    $('#keywords').jQCloud([], {
-    	  width: 500,
-    	  height: 350
-	});
+//    $('#keywords').jQCloud([], {
+//    	  width: 500,
+//    	  height: 350
+//	});
     
     /**
      * Get Tag data
@@ -17,18 +17,60 @@ $(document).ready(function() {
     		success: function(data) {
     			viewTimer = null;
     			var words = [];
-    			data.forEach(function(d) {words.push({text: d.name, weight: d.value})});
+//    			data.forEach(function(d) {words.push({text: d.name, weight: d.value})});
 //    			for (var key in data) {
 //    				words.push({text: key, weight: data[key]});
 //    			}
-    			$('#keywords').jQCloud('update', words);
-    			console.log('data', data);
+//    			$('#keywords').jQCloud('update', words);
+    			createCloud(data);
+//    			console.log('data', data);
         	}
     	});
     }
     getWordData();
 //    setInterval(getWordData,5*60*1000);
-    setInterval(getWordData,10*1000);
+    setInterval(getWordData, 5*60*1000);
 
+    function createCloud(data) {
+    	var chart = echarts.init(document.getElementById('keywords'));
+    	chart.setOption({
+    	    series: [{
+    	        type: 'wordCloud',
+    	        shape: 'circle',
+    	        left: 'center',
+    	        top: 'center',
+    	        width: '70%',
+    	        height: '100%',
+    	        right: null,
+    	        bottom: null,
+    	        sizeRange: [24, 100],
+    	        rotationRange: [-90, 90],
+    	        rotationStep: 45,
+    	        gridSize: 10,
+    	        drawOutOfBound: false,
+    	        textStyle: {
+    	            normal: {
+    	                fontFamily: 'sans-serif',
+    	                fontWeight: 'bold',
+    	                // Color can be a callback function or a color string
+    	                color: function () {
+    	                    // Random color
+    	                    return 'rgb(' + [
+    	                        Math.round(Math.random() * 160),
+    	                        Math.round(Math.random() * 160),
+    	                        Math.round(Math.random() * 160)
+    	                    ].join(',') + ')';
+    	                }
+    	            },
+    	            emphasis: {
+    	                shadowBlur: 10,
+    	                shadowColor: '#333'
+    	            }
+    	        },
+    	        data: data
+    	    }]
+    	});
+    	
+    }
     
 });
